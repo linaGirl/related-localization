c4e750eba37c06e12ab19d8c9b166e48a343b0f6
Michael van der Weg <michael@joinbox.com>
2014-08-30 19:14:15 +0200

    first version, readonly support

--- test.js
+++ test.js
@@ -3,38 +3,30 @@
     var Class       = require('ee-class')
         , log       = require('ee-log')
         , async     = require('ee-async')
-        , ORM       = require('ee-orm')
+        , ORM       = require('../ee-orm')
         , project   = require('ee-project')
         , Extension = require('./');
 
     var orm = new ORM(project.config.db);
 
-    orm.use(new Extension());
+    orm.use(new Extension({orm:orm}));
+
 
     orm.on('load', function(err) {
         log('orm loaded');
-        var   db = orm.ee_orm_nestedset_test
+        var   db = orm.ee_orm_localization_test
             , start;
    
 
         var done = function(err, data){
             if (err) log(err);
             if (data && data.dir) data.dir();
+            log(data);
         }
 
 
-        db.tree({id: 5}).findOne(function(err, node) {
-            if (err) done(err);
-            else {
-                node.setParent(3).save(function(err, movedNode) {
-                    if (err) done(err);
-                    else {
-                        assert.equal(movedNode.left, 2);
-                        assert.equal(movedNode.right, 3);
-                        done();
-                    }
-                });
-            }
-        });
-   
+        db.event(['*']).setLocale(['nl', 'en']).find(function(err, list) {
+            log(err, list, list.length);
+
+        });   
     });
